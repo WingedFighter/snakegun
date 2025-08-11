f2f50c57aded61b7776a20523e797b4a790013f4
Ryne9 <rynegipson@gmail.com>
2025-08-11 15:55:21 -0400

    Hurtbox and platformer stuff

--- platformer/components/health_component.gd
+++ platformer/components/health_component.gd
@@ -1,13 +1,24 @@
+class_name HealthComponent
 extends Node2D
 
 @export var maxHealth: float
 var currentHealth: float
 
+signal health_depleted
+signal health_lost
+signal health_increased
+
 func _ready() -> void:
 	currentHealth = maxHealth
 
 func hurt(damage: float) -> void:
+	if (damage < 0):
+		health_lost.emit()
+	if (damage > 0):
+		health_increased.emit()
 	currentHealth -= damage
+	if (currentHealth <= 0):
+		health_depleted.emit()
 	
 func heal(health: float) -> void:
 	hurt(-health)
---
+++ platformer/components/hurtbox_component.gd
@@ -0,0 +1,25 @@
+class_name HurtboxComponent
+extends Area2D
+
+@export var healthComponent: HealthComponent
+@export var iframe_timer: Timer
+
+var iframe_time: float = 0.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area_entered.connect(take_damage)
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func take_damage(damage: float) -> void:
+	# Iframes are an optional component, cannot be damaged when there is no iframe timer associated.
+	if iframe_timer != null:
+		if iframe_timer.is_stopped():
+			healthComponent.hurt(damage)
+			iframe_timer.start(iframe_time)
+	else:
+		healthComponent.hurt(damage)
