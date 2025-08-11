3ee2b52820f12da2caa2b90aab54982b29ea2927
WingedFighter <mrjohnston1995@gmail.com>
2025-08-10 19:42:53 -0600

    Adding better title, settings menu

---
+++ ui/scripts/escape_menu.gd
@@ -0,0 +1,102 @@
+extends Control
+
+@onready var resolution_button: OptionButton = $Panel/MarginContainer/TabContainer/ItemList/VBoxContainer/Resolution/OptionButton
+@onready var fullscreen_button: CheckBox = $Panel/MarginContainer/TabContainer/ItemList/VBoxContainer/Fullscreen/CheckBox
+@onready var keybindings_button: Button = $Panel/MarginContainer/TabContainer/ItemList/VBoxContainer/KeybindingsButton
+@onready var save_button: Button = $Panel/MarginContainer/TabContainer/ItemList/VBoxContainer/SaveButton
+@onready var return_button: Button = $Panel/MarginContainer/TabContainer/ItemList/VBoxContainer/ReturnButton
+@onready var close_button: Button = $Panel/MarginContainer/TabContainer/ItemList/VBoxContainer/CloseButton
+@onready var exit_button: Button = $Panel/MarginContainer/TabContainer/ItemList/VBoxContainer/ExitButton
+
+@onready var keybindings_panel: Control = $KeybindingPanel
+
+@export var resolutions: Dictionary = {
+	"640x360": Vector2(640, 360),
+	"1280x720": Vector2(1280, 720),
+	"1920x1080": Vector2(1920, 1080),
+	"2560x1440": Vector2(2560,1440),
+	"3840x2160": Vector2(3840, 2160)
+}
+
+@export var music_bus: int
+@export var main_menu_scene: String = "res://ui/main_title.tscn"
+
+@export var main_menu: bool
+
+var default_resolution = "1280x720"
+
+func _ready() -> void:
+	visible = false
+	add_resolutions()
+	find_and_set_current_window_size()
+	set_to_fullscreen_state()
+
+	fullscreen_button.toggled.connect(on_fullscreen_pressed)
+	resolution_button.item_selected.connect(on_resolution_item_selected)
+	keybindings_button.pressed.connect(on_keybindings_pressed)
+	save_button.pressed.connect(on_save_pressed)
+	return_button.pressed.connect(on_return_pressed)
+	close_button.pressed.connect(on_close_pressed)
+	exit_button.pressed.connect(on_exit_pressed)
+	
+	if main_menu:
+		save_button.visible = false
+		return_button.visible = false
+		exit_button.visible = false
+
+func add_resolutions() -> void:
+	for res in resolutions:
+		resolution_button.add_item(res)
+
+func find_and_set_current_window_size() -> void:
+	var current_window_size = str(get_window().size.x) + "x" + str(get_window().size.y)
+	var index = resolutions.keys().find(current_window_size)
+
+	if index >= 0:
+		resolution_button.selected = index
+	else:
+		index = resolutions.keys().find(default_resolution)
+		resolution_button.selected = index
+		on_resolution_item_selected(index)
+
+func on_resolution_item_selected(index) -> void:
+	if index >= 0:
+		var res = resolution_button.get_item_text(index)
+		get_window().size = resolutions[res]
+
+func open() -> void:
+	visible = true
+	get_tree().paused = true
+
+func close() -> void:
+	on_close_pressed()
+
+func set_to_fullscreen_state() -> void:
+	on_fullscreen_pressed(fullscreen_button.button_pressed)
+
+func on_keybindings_pressed() -> void:
+	keybindings_panel.visible = true
+
+func on_fullscreen_pressed(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func on_return_pressed() -> void:
+	get_tree().change_scene_to_file(main_menu_scene)
+
+func on_save_pressed() -> void:
+	pass
+
+func on_close_pressed() -> void:
+	visible = false
+	keybindings_panel.visible = false
+	get_tree().paused = false
+
+func on_exit_pressed() -> void:
+	get_tree().paused = false
+	get_tree().quit()
+
+func _on_h_slider_value_changed(_value: float) -> void:
+	pass
---
+++ ui/scripts/main_title.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@export_file("*.tscn") var new_game_scene: String
+@export_file("*.tscn") var continue_game_scene: String
+
+@onready var new_game_button: Button = $NewGameButton
+@onready var continue_button: Button = $ContinueButton
+@onready var settings_button: Button = $SettingsButton
+@onready var quit_button: Button = $QuitButton
+@onready var escape_menu: Control = $EscapeMenu
+
+func _physics_process(_delta: float) -> void:
+	new_game_button.pressed.connect(new_game)
+	continue_button.pressed.connect(continue_game)
+	settings_button.pressed.connect(settings)
+	quit_button.pressed.connect(quit_game)
+
+	# Unpause if paused
+	get_tree().paused = false
+	# Defaults to overworld, so disable physics
+	set_physics_process(false)
+
+func new_game() -> void:
+	get_tree().change_scene_to_file(new_game_scene)
+
+func continue_game() -> void:
+	get_tree().change_scene_to_file(continue_game_scene)
+
+func settings() -> void:
+	escape_menu.open()
+
+func quit_game() -> void:
+	get_tree().quit()
