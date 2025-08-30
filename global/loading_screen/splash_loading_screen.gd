7ddaca7a8ff245ddc6e748c8f3cd54f4acf0aa0a
WingedFighter <mrjohnston1995@gmail.com>
2025-08-30 16:12:47 -0600

    most of day 3, except explosion/killing/credits

--- global/loading_screen/splash_loading_screen.gd
+++ global/loading_screen/splash_loading_screen.gd
@@ -30,7 +30,12 @@ extends CanvasLayer
 	"PreBoss22": "res://overworld/cutscenes/pre_boss_2/pre_boss_22.tscn",
 	"PostBoss2": "res://overworld/cutscenes/post_boss_2/post_boss_2.tscn",
 	"ThirdSchool1": "res://overworld/cutscenes/third_steps/third_school_1.tscn",
-	"ThirdSchool2": "res://overworld/cutscenes/third_steps/third_school_2.tscn"
+	"ThirdSchool2": "res://overworld/cutscenes/third_steps/third_school_2.tscn",
+	"BrightLights": "res://overworld/cutscenes/bright_lights/bright_lights.tscn",
+	"PreBoss3": "res://overworld/cutscenes/pre_boss_3/pre_boss_3.tscn",
+	"PostBoss3": "res://overworld/cutscenes/post_boss_3/post_boss_3.tscn",
+	"Killed": "",
+	"Nuked": ""
 }
 
 @export var music_dictionary: Dictionary[String, String] = {
---
+++ overworld/cutscenes/post_boss_3/post_boss_3.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+const VELOCITY = 10.0
+
+@export var change_scene_2: String = "Nuked"
+@export var change_scene_1: String = "Killed"
+
+@onready var player: Player25D = %Player25D
+@onready var skip_button: Button = $CanvasLayer/SkipButton
+@onready var talk_manager: TalkManager = $Conversation/TalkManager
+
+var start_conversation: bool = false
+var frame_count: int = 0
+var frame_limit: int = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	State.flags['in_cutscene'] = true
+	State.flags['start_conversation'] = false
+	player.is_paused = true
+	skip_button.pressed.connect(end_cutscene)
+
+func _process(_delta: float) -> void:
+	if frame_count < frame_limit:
+		frame_count += 1
+	elif !start_conversation:
+		State.flags['start_conversation'] = true
+		var interact_event = InputEventAction.new()
+		interact_event.action = "interact"
+		interact_event.pressed = true
+		Input.parse_input_event(interact_event)
+		start_conversation = true
+	elif !player.is_paused:
+		end_cutscene()
+
+func end_cutscene() -> void:
+	State.flags['in_cutscene'] = false
+	State.flags.erase('start_conversation')
+	if len(talk_manager.talk_state.flags) == 0 || talk_manager.talk_state.flags['did_it'] == 'true':
+		SceneManager.change_scene(change_scene_2)
+	else:
+		SceneManager.change_scene(change_scene_1)
