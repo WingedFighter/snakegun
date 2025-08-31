e60c0573fc42abe4fb6d04374b8601a216051670
Ryne9 <rynegipson@gmail.com>
2025-08-31 15:12:08 -0400

    Add tutorial scene with shoot signal

--- platformer/components/shoot_component.gd
+++ platformer/components/shoot_component.gd
@@ -2,8 +2,12 @@ class_name ShootComponent
 extends Node2D
 
 @export var damage: float = 1
+@export var accuracy: float = 0
 @onready var bullet = preload("res://platformer/components/bullet.tscn")
 var root: Window
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+
+signal shot
 
 func _ready():
 	root = get_tree().root
@@ -17,4 +21,7 @@ func fire(velocity: Vector2, d: float) -> void:
 		fired_bullet.damage = damage
 	fired_bullet.global_position = global_position
 	fired_bullet.v = velocity
+	if accuracy != 0:
+		fired_bullet.v.y = rng.randf_range(-accuracy, accuracy)
 	root.add_child(fired_bullet)
+	shot.emit()
