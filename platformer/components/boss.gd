9a1e48d113887509cbde50254fecd2279c492334
Ryne9 <rynegipson@gmail.com>
2025-09-02 16:05:38 -0400

    Add boss 2 and 3 assets and scenes

--- platformer/components/boss.gd
+++ platformer/components/boss.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var sprite: AnimatedSprite2D = $HitflashComponent/AnimatedSprite2D
 @onready var timer: Timer = $DeathTimer
+@export var boss_level: int = 1
 
 func _ready() -> void:
 	health_component.health_depleted.connect(on_death)
@@ -16,4 +17,4 @@ func on_death() -> void:
 	timer.paused = false
 
 func scene_transition() -> void:
-	SceneManager.change_scene("PostBoss1")
+	SceneManager.change_scene("PostBoss" + str(boss_level))
