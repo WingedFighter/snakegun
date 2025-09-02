618cf1af2f7829276aeded5fc0d1d65ed918fe0f
WingedFighter <mrjohnston1995@gmail.com>
2025-09-01 19:35:34 -0600

    threaded in boss level

--- global/loading_screen/splash_loading_screen.gd
+++ global/loading_screen/splash_loading_screen.gd
@@ -24,6 +24,7 @@ extends CanvasLayer
 	"PostTutorial": "res://overworld/cutscenes/post_tutorial/post_tutorial.tscn",
 	"Level1": "res://platformer/scenes/level1.tscn",
 	"PreBoss1": "res://overworld/cutscenes/pre_boss_1/pre_boss_1.tscn",
+	"Boss1": "res://platformer/scenes/boss1.tscn",
 	"PostBoss1": "res://overworld/cutscenes/post_boss_1/post_boss_1.tscn",
 	"FatDump": "res://overworld/cutscenes/fat_dump/fat_dump.tscn",
 	"Level2": "res://platformer/scenes/level2.tscn",
