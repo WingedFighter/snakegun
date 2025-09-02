76d43fe6ad6622b9f9803a94bcc3111eefad0142
Ryne9 <rynegipson@gmail.com>
2025-09-01 21:21:24 -0400

    Boss 1 scene and HUD changes

---
+++ platformer/components/boss.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var sprite: AnimatedSprite2D = $HitflashComponent/AnimatedSprite2D
+@onready var timer: Timer = $DeathTimer
+
+func _ready() -> void:
+	health_component.health_depleted.connect(on_death)
+	timer.autostart = false
+	timer.one_shot = true
+	timer.timeout.connect(scene_transition)
+
+func on_death() -> void:
+	sprite.play("death")
+	timer.start(1)
+	timer.paused = false
+
+func scene_transition() -> void:
+	SceneManager.change_scene("PostBoss1")
