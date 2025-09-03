0ef1b263c58aad06f6b4124e67e6163866b5e40c
Ryne9 <rynegipson@gmail.com>
2025-09-02 23:55:17 -0400

    tutorial

--- platformer/scenes/tutorial.gd
+++ platformer/scenes/tutorial.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var to_scene: String = "PostTutorial"
 
 @onready var shoot_component = $Player/ShootComponent
+@onready var shoot_label = $FirstTimePlaying/ShootLabel
 
 var num_shots = 0
 
@@ -14,5 +15,6 @@ func _ready() -> void:
 
 func on_shot() -> void:
 	num_shots += 1
+	shoot_label.text = "Shoot " + str(num_shots) + "/" + str(shots_to_transition)
 	if num_shots >= shots_to_transition:
 		SceneManager.change_scene(to_scene)
