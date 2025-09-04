c482c423886f6e426500ce8bb07403db086d2ed5
Ryne9 <rynegipson@gmail.com>
2025-09-03 22:20:36 -0400

    Add SFX bus and multiple SFX

--- global/audio/audio_manager.gd
+++ global/audio/audio_manager.gd
@@ -5,17 +5,13 @@ var stopped: bool = false
 var music_dictionary: Dictionary[String, AudioStream]
 var sfx_dictionary: Dictionary[String, AudioStream]
 var background_music: AudioStreamPlayer
-var sfx: AudioStreamPlayer
 var current_track: String
 
 func _ready() -> void:
 	background_music = AudioStreamPlayer.new()
 	add_child(background_music)
-	sfx = AudioStreamPlayer.new()
-	add_child(sfx)
 
 	background_music.bus = "Music"
-	sfx.bus = "Sfx"
 
 func play_music(song: String) -> void:
 	if background_music && current_track != song:
@@ -25,9 +21,12 @@ func play_music(song: String) -> void:
 		background_music.play()
 
 func play_sfx(sound: String) -> void:
-	if sfx:
-		sfx.stream = sfx_dictionary[sound]
-		sfx.play()
+	var sfx: AudioStreamPlayer = AudioStreamPlayer.new()
+	add_child(sfx)
+	sfx.bus = "Sfx"
+	sfx.stream = sfx_dictionary[sound]
+	sfx.play()
+	sfx.finished.connect(on_sfx_finished.bind(sfx))
 
 func stop_music() -> void:
 	if background_music:
@@ -36,3 +35,6 @@ func stop_music() -> void:
 
 func get_current_track_name() -> String:
 	return current_track
+
+func on_sfx_finished(sfx: AudioStreamPlayer) -> void:
+	sfx.queue_free()
